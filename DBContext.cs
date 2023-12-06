9c0bc20ffe2982c3a8867540b47405db98ad9e97
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-12-06 11:05:55 +0400

    Updated the files in project

---
+++ DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sportShop
+{
+    internal class DBContext
+    {
+        public class User
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class ApplicationContext : DbContext
+        {
+            public DbSet<User> Users { get; set; } = null!;
+
+            public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=пароль_от_postgres");
+            }
+        }
+    }
+}
