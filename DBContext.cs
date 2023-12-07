e956c98cf61337b35bca34b6cdf43673494ab1a0
Илья Красненков <krasnenkov.ilya@inbox.ru>
2023-12-07 13:02:13 +0400

    Updated the connect database

--- DBContext.cs
+++ DBContext.cs
@@ -1,24 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace sportShop
 {
-    internal class DBContext
+  internal class DBContext
+  {
+    /* подключение базы данных */
+    public class ApplicationContext : DbContext
     {
-        public class User
-        {
-            public int Id { get; set; }
-            public string? Name { get; set; }
-            public int Age { get; set; }
-        }
-
-        public class ApplicationContext : DbContext
-        {
-            public DbSet<User> Users { get; set; } = null!;
+      public DbSet<User> Users { get; set; } = null!;
  
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=52");
-            }
-        }
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=12345Qq");
     }
+
+    #region Tabels
+    public class User
+    {
+      [Key]
+      public int Id { get; set; }
+      public string? Name { get; set; }
+      public int Age { get; set; }
+    }
+
+
+    #endregion
+  }
 }
